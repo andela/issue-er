d0e3705482730b91a88ebd4ca0c78aa4661011b6
Paul Dariye <paul.dariye@gmail.com>
2017-09-18 12:00:12 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,143 @@
+const { json, send } = require('micro')
+// const cors = require('micro-cors')()
+const { GraphQLClient, request } = require('graphql-request')
+const crypto = require('crypto')
+
+
+const endpoint = 'https://api.github.com/graphql'
+const token = process.env.GH_TOKEN
+const secret = process.env.GH_WEBHOOK_SECRET
+const owner = process.env.GH_OWNER
+const repository = process.env.GH_REPOSITORY
+
+
+// if (!token && !secret) {
+//     throw Error(`Missing 'GH_TOKEN' && 'GH_WEBHOOK_SECRET' env variables`)
+// }
+
+
+const client = new GraphQLClient(endpoint, {
+  headers: {
+    Authorization: `bearer ${token}`,
+  },
+})
+
+
+function signRequestBody(key, body) {
+  return `sha1=${crypto.createHmac('sha1', key.toString()).update(body.toString(), 'utf-8').digest('hex')}`
+}
+
+module.exports = async function (req, res) {
+  
+  const payload = await json(req)
+  const headers = req.headers
+  const sig = headers['x-hub-signature']
+  const githubEvent = headers['x-github-event']
+  const id = headers['x-github-delivery']
+  const issueNumber = payload.issue.number
+  // const calculateSig = signRequestBody(secret, payload) 
+  let issueID
+  let errorMessage
+
+  if (typeof token !== 'string') {
+    errorMessage = `Must provide a 'GH_WEBHOOK_SECRET' env variable`
+    return send(res, 401, { 
+      headers: { 'Content-Type': 'text/plain' },
+      body: errorMessage }) 
+  }
+
+  if (!sig) {
+    errorMessage = `No X-Hub-Signature found on request`
+    return send(res, 401, { 
+      headers: { 'Content-Type': 'text/plain' },
+      body: errorMessage }) 
+  }
+
+  if (!githubEvent) {
+    errorMessage = `No Github Event found on request`
+    return send(res, 401, { 
+      headers: { 'Content-Type': 'text/plain' },
+      body: errorMessage }) 
+  }
+
+  if (githubEvent !== 'issues') {
+    errorMessage = `No Github Issues event found on request`
+    return send(res, 200, { 
+      headers: { 'Content-Type': 'text/plain' },
+      body: errorMessage }) 
+  }
+
+  if (payload.action !== 'opened') {
+    errorMessage = `Only opened action on issues please :)`
+    return send(res, 200, { 
+      headers: { 'Content-Type': 'text/plain' },
+      body: errorMessage }) 
+  }
+  // if (sig !==  calculateSig) {
+  //   errorMessage = `X-Hub-Signature incorrect. Github webhook token doesn't match`
+  //   return send(res, 401, { 
+  //     headers: { 'Content-Type': 'text/plain' },
+  //     body: errorMessage }) 
+  // }
+
+
+  console.log(`------------------------------`)
+  console.log(`Github-Event:  ${githubEvent} with action: ${payload.action}`)
+  console.log(`-------------------------------`)
+  const variables = {
+    "owner": owner,
+    "name": repository,
+    "issueNumber": issueNumber,
+    "issue": {
+      "contentId": issueID,
+      "projectColumnId": "MDEzOlByb2plY3RDb2x1bW4xNDc0MzM4"
+    } 
+  }
+
+    // FindIssueId
+  const FindIssueID = `
+    query FindIssueID($owner: String!, $name: String!, $issueNumber: Int!) {
+      repository(owner: $owner, name: $name) {
+        issue(number: $issueNumber) {
+          id
+        }
+      }
+    }
+  `
+
+  const data = await client.request(FindIssueID, variables)
+  issueID = data.repository.issue.id
+  variables.issue.contentId = issueID
+  
+  // AddIssueToCard
+  
+  const AddIssueToProject = `
+    mutation AddIssueToProject($issue: AddProjectCardInput!) {
+      addProjectCard(input: $issue) {
+        cardEdge {
+          node {
+            id
+          }
+        }
+        projectColumn {
+          id
+        }
+      }
+    }
+  `
+
+  if (!issueID) {
+    return send(res, 200, {  headers: { 'Content-Type': 'text/plain' }, body: 'No issue with ID found' })
+  }
+
+  try {
+      addIssueToProjectPayload = await client.request(AddIssueToProject, variables)
+    if (!addIssueToProjectPayload) {
+      return send(res, 200, {  headers: { 'Content-Type': 'text/plain' }, body: 'Project already has the associated issue' })
+    }
+  } catch (error) {
+      return send(res, 401, {  headers: { 'Content-Type': 'text/plain' }, body: 'Error adding issue to project' })
+  }
+
+  return send(res, 200, { headers: { 'Content-Type':'application/json' }, body: `Done: \n${addIssueToProjectPayload}` })
+}
