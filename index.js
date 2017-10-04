7177159cda12592ac174043919ffd297e1e5d399
Paul Dariye <paul.dariye@gmail.com>
2017-10-04 00:03:44 -0400

    Use appropriate GitHub labels to update Airtable. Assign GitHub project based on department that submits request via Airtable. Update startDate field in Airtable when GitHub Issue is labeled accepted Update dateDelivered field in Airtable when GitHub Issue is labeled completed. Include complete Airtable jobID (xxx-xxx-xxx) prominently in GitHub description

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 require('dotenv-safe').load()
 
-const { json, send } = require('micro')
-const { GraphQLClient, request } = require('graphql-request')
 const crypto = require('crypto')
+const { json, send, text } = require('micro')
+const { GraphQLClient } = require('graphql-request')
+const airtable = require('airtable')
+const dateFormat = require('dateformat')
 
 // Env Variables
 const token = process.env.GH_TOKEN
 const secret = process.env.GH_WEBHOOK_SECRET
 const owner = process.env.GH_OWNER
-const repository = process.env.GH_REPOSITORY
+const name = process.env.GH_REPOSITORY
 const endpoint = process.env.GH_ENDPOINT
+const airtableKey = process.env.AIRTABLE_API_KEY
+const airtableBase = process.env.AIRTABLE_BASE
+const table = process.env.AIRTABLE_TABLE
+const view = process.env.AIRTABLE_VIEW_ENDPOINT
 
 // Initialize GraphQLClient
 const client = new GraphQLClient(endpoint, {
@@ -18,26 +24,21 @@ const client = new GraphQLClient(endpoint, {
   },
 })
 
-// Initialize query and mutation variable object
-const baseVariables = {
-  "owner": owner,
-  "name": repository,
-}
+// Initialize Airtable client
+airtable.configure({
+  apiKey: airtableKey
+})
 
-function signRequestBody(key, body) {
-  return `sha1=${crypto.createHmac('sha1', key.toString()).update(body.toString(), 'utf-8').digest('hex')}`
-}
+const base = airtable.base(airtableBase)(table)
 
-
-// function performOperation(...args) {
-//   const { operation, variables } = args
-//   return  await client.request(operation, variables)
-// }
+// Create query and mutation variable object
+const baseVariables = { owner, name }
 
 
+// Issue status labels
+const statuses = ['incoming', 'accepted', 'in progress', 'rejected', 'blocked', 'review', 'completed', 'hold', 'canceled']
 
 // Grapql Operations: Queries & Mutations
-
 const operations = {
   FindProjectColumnID: `
     query FindProjectByName($owner: String!, $name: String!, $projectName: String!) {
@@ -49,6 +50,27 @@ const operations = {
                 edges {
                   node {
                     id
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+  FindProjectColumnIDs: `
+    query FindProjectColumnIDs($owner:String!, $name:String!, $projectName:String!) {
+      repository(owner:$owner,name:$name) {
+        projects(first:1, search:$projectName){
+          edges {
+            node {
+              columns(first:7) {
+                totalCount
+                edges {
+                  node {
+                    id
+                    name
                   }
                 }
               }
@@ -93,85 +115,158 @@ const operations = {
         clientMutationId
       }
     }
+  `,
+  AddComment: `
+    mutation AddComment($issue: AddCommentInput!) {
+      addComment(input:$issue) {
+        commentEdge {
+          node {
+            id
+          }
+        }
+        clientMutationId
+      }
+    }
   `
 }
 
+
+function signRequestBody(key, body) {
+  return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`
+}
+
 module.exports = async function (req, res) {
-  if (!req) { return }  
-  const payload = await json(req)
-  const headers = req.headers
-  const sig = headers['x-hub-signature']
-  const githubEvent = headers['x-github-event']
-  const id = headers['x-github-delivery']
-  let operationPayload = {}
-  let errorMessage
 
-  if (!sig) {
-    errorMessage = `No X-Hub-Signature found on request`
-    return send(res, 401, { 
-      headers: { 'Content-Type': 'text/plain' },
-      body: errorMessage }) 
-  }
+  try {
+    const payload = await json(req) 
+    const body = await text(req) 
+    const headers = req.headers
+    const sig = headers['x-hub-signature']
+    const githubEvent = headers['x-github-event']
+    const id = headers['x-github-delivery']
+    const calculatedSig = signRequestBody(secret, body)
+    let errMessage
 
-  if (!githubEvent) {
-    errorMessage = `No Github Event found on request`
-    return send(res, 401, { 
-      headers: { 'Content-Type': 'text/plain' },
-      body: errorMessage }) 
-  }
-
-  if (githubEvent !== 'issues') {
-    errorMessage = `No Github Issues event found on request`
-    return send(res, 200, { 
-      headers: { 'Content-Type': 'text/plain' },
-      body: errorMessage }) 
-  }
-
-  if (payload.action !== 'opened') {
-    errorMessage = `Only 'opened' action permitted at the momment`
-    return send(res, 200, { 
-      headers: { 'Content-Type': 'text/plain' },
-      body: errorMessage }) 
-  }
-
-  payload.issue.labels.forEach(async (label) => {    
-    const issueNumber = payload.issue.number
-    const projectName = label.name === 'incoming' ? 'All Projects' : label.name
-
-    const variables = Object.assign({}, baseVariables, {
-      "issueNumber": issueNumber,
-      "projectName": projectName
-    })
-
-    let data
-    data  = await client.request(operations.FindIssueID, variables)
-    const issueID = data.repository.issue.id
-    data  = await client.request(operations.FindProjectColumnID, variables)
-    const columnID = data.repository.projects.edges[0].node.columns.edges[0].node.id
-
-    if(issueID && columnID) {
-
-      let mutationVariables = Object.assign({}, variables, {
-        "issue": {
-          "contentId": issueID,
-          "projectColumnId": columnID
-        }
-      })
-    
-      try {
-        mutationPayload = await client.request(operations.AddProjectCard, mutationVariables)
-      } catch (error) {
-        return send(res, 401, {  headers: { 'Content-Type': 'text/plain' }, body: 'Error adding issue to project' })
-      }
-
-      operationPayload = Object.assign({}, mutationPayload)
-
-    } else {
-        return send(res, 401, {  headers: { 'Content-Type': 'text/plain' }, body: 'No matching labels for project board' })
+    if (!sig) {
+      errMessage = 'No X-Hub-Signature found on request'
+      return send(res, 401, { 
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMessage }) 
     }
 
-    return "Done"
-  })
+    if (!githubEvent) {
+      errMessage = 'No Github Event found on request'
+      return send(res, 422, { 
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMessage }) 
+    }
 
-  return send(res, 200, { headers: { 'Content-Type':'application/json' }, body: `Done with response: ${JSON.stringify(operationPayload)}` })
+    if (githubEvent !== 'issues') {
+      errMessage = 'No Github Issues event found on request'
+      return send(res, 200, { 
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMessage }) 
+    }
+
+    if(!id) {
+      errMessage = 'No X-Github-Delivery found on request'
+      return send(res, 401, { 
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMessage }) 
+    }
+
+    if (sig !== calculatedSig) {
+      errMessage = 'No X-Hub-Signature doesn\'t match Github webhook secret'
+      return send(res, 401, { 
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMessage }) 
+    }
+
+    const action = payload.action
+    
+    if (action === 'opened') {
+
+      const issueNumber = payload.issue.number
+      const variables =  Object.assign({}, baseVariables, { issueNumber })
+
+      const issue = await client.request(operations.FindIssueID, variables)
+      const subjectId = issue.repository.issue.id
+        
+      const record = await base.select({ filterByFormula: `githubIssue = ${issueNumber}`}).firstPage()
+      const recordID = record[0].getId()
+      const requestID = record[0].get('requestID')
+      const recordView = view + recordID
+
+      if (subjectId) {
+
+        const body = `## Request ID: [${requestID}](${recordView}) \r\n`
+
+        const mutationVariables = Object.assign({}, variables, {
+          "issue": { subjectId , body }
+        })
+
+        await client.request(operations.AddComment, mutationVariables)
+      }
+    }
+
+    if (action === 'labeled') {
+      const label = payload.label
+      const status = label.name
+      const issueNumber = payload.issue.number
+      const projectName = label.name === 'incoming' ? 'All Projects' : label.name
+      
+      // Lookup airtable record by issueNumber 
+      const record = await base.select({ filterByFormula: `githubIssue = ${issueNumber}` }).firstPage()
+      const recordID = record[0].getId()
+      const recordJobStatus = record[0].get('jobStatus')
+
+      // Update airtable jobStatus field with label if airtable record exists &&
+      // Label status is in statuses && record status is not the same as label name
+      if (recordID && statuses.includes(status) && status !== recordJobStatus) {
+
+        await base.update(recordID, { 'jobStatus': status })
+
+        const today = dateFormat(new Date(), 'isoDate')
+
+        if (status === 'accepted') {
+          await base.update(recordID, { 'startDate': today })
+        }
+
+        if (status === 'completed') {
+          await base.update(recordID, { 'dateDelivered': today })
+        }
+
+      }
+
+      // Add issue to project board
+      const variables =  Object.assign({}, baseVariables, {
+        issueNumber,
+        projectName
+      })
+
+      const issue = await client.request(operations.FindIssueID, variables)
+      const contentId = issue.repository.issue.id
+
+      const project = await client.request(operations.FindProjectColumnID, variables)
+
+      if (project.repository.projects.edges.length === 1 && payload.sender.login === 'studiobot' ) {
+
+        const projectColumnId = project.repository.projects.edges[0].node.columns.edges[0].node.id
+      
+        if (contentId && projectColumnId) {
+
+          const projectCardMutationVariables = Object.assign({}, variables, {
+            "issue": { contentId, projectColumnId }
+          })
+
+          await client.request(operations.AddProjectCard, projectCardMutationVariables)
+        }
+      }
+    }
+
+    return send(res, 200, { body: `Done with action: '${action}'` })
+  } catch(err) {
+    console.log(err)
+    send(res, 500)  
+  }
 }
