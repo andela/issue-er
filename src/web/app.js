6b2f5c651ad6b96f5571f49f69347d851df2de9f
Paul Dariye <paul.dariye@gmail.com>
2018-04-05 13:29:25 -0400

    Refactor timer function for jobs

--- src/web/app.js
+++ src/web/app.js
@@ -1,3 +1,4 @@
+const cluster = require('cluster')
 const crypto = require('crypto')
 const { json, send, text } = require('micro')
 const CronJob = require('cron').CronJob
@@ -70,19 +71,21 @@ module.exports = async (req, res) => {
 
     const now = moment()
     const tz = moment.tz.guess()
-    const inFifteenMinutes = now.clone().add(15, 'minutes')
-    const inOneMinute = now.clone().add(1, 'minute')
+    const time = (number, unit) =>
+      now.clone().add(number, unit).toDate()
+    
+    if (cluster.isMaster) {
+      const job = new CronJob({
+        cronTime: (action === 'opened' || action === 'closed')
+        ? time(15, 'minutes')
+        : time(1, 'minute'),
+        onTick: actions[action](payload),
+        start: false,
+        timeZone: tz
+      })
 
-    const job = new CronJob({
-      cronTime: (action === 'opened' || action === 'closed')
-      ? inFifteenMinutes.toDate()
-      : inOneMinute.toDate(),
-      onTick: actions[action](payload),
-      start: false,
-      timeZone: tz
-    })
-
-    job.start()
+      job.start()
+    }
 
     return send(res, 200, {
       body: `Scheduled '${action}' job for issue: '${payload.issue.number}'`
